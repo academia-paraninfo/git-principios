2eb4e8ed08be47ae94403af0347f47b825a9694b
Nacho Geoghegan <ignaciog00@gmail.com>
2024-05-06 19:28:30 -0300

    Squashed commit of the following:
    commit 8677eb4d3eb4a3f8c5d44673a2cf4bb5614ba6ab
    Author: Nacho Geoghegan <ignaciog00@gmail.com>
    Date:   Mon May 6 19:25:09 2024 -0300
    
        added readme
    
    commit 663e31aec2b8ec66fee5ee9d58dd8a67dbb9bf3a
    Merge: 93c242a f86831e
    Author: Nacho Geoghegan <54043072+nachog00@users.noreply.github.com>
    Date:   Mon May 6 19:06:10 2024 -0300
    
        Merge pull request #1 from academia-paraninfo/dev
    
        completamos fase 1
    
    commit f86831ebf501bd8a2a2bafa586eaf7068d01ca42
    Author: Nacho Geoghegan <ignaciog00@gmail.com>
    Date:   Mon May 6 18:46:43 2024 -0300
    
        arreglamos otro bug en dev
    
    commit 93c242a2a443bff4e9ab49deff26ffa16ff1fba5
    Merge: 5002bcb ab8f16a
    Author: Nacho Geoghegan <ignaciog00@gmail.com>
    Date:   Mon May 6 18:42:48 2024 -0300
    
        Merge branch 'f-lo-hace-nacho' into dev
    
    commit 5002bcb5f122c42b1372df76bd8eaf24623c939f
    Author: Nacho Geoghegan <ignaciog00@gmail.com>
    Date:   Mon May 6 18:39:17 2024 -0300
    
        resolvemos otro bug

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log("hello country")
 console.log("hello from dev branch")
 
 console.log("resolvemos un bug")
+console.log("resolvemos otro bug")
 
 console.log("nacho avanza en su feature")
 console.log("mas avance")
@@ -12,3 +13,5 @@ console.log("termina")
 
 console.log("sigue con otra fase")
 
+
+console.log("arreglamos otro bug en dev")
