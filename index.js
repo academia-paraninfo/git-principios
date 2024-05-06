72b3f63361e0cd0953dd7eb2c5be482ce6843493
Nacho Geoghegan <ignaciog00@gmail.com>
2024-05-06 19:12:09 -0300

    hago mas cosas

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ console.log("hello from dev branch")
 
 console.log("samuel trabaja en algun feature")
 console.log("samuel todavia en algun feature")
+
+console.log("hago mas cosas")
+console.log("hago mas cosas")
